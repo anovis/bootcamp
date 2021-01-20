3dcb585e6aeb7ff48dbff5440e1d598b715f6b9f
austen <anovis@ymail.com>
2021-01-19 21:06:06 -0500

    adding exercise1

---
+++ python/exercise1-intro/exercise1.py
@@ -0,0 +1,20 @@
+# Exercise 1
+
+# To run this program go to the terminal and use the command `python exercise1.py`
+
+print("Welcome to Python")
+
+print('Enter your name:')
+
+x = input()
+
+print('Hello, ' + x)
+
+# TODO:
+
+# Get the users age and determine what year they were born.
+
+# print('How old are you?')
+
+
+# print('So you were born in' + x)
